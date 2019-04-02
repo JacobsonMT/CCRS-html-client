582b56f1f162e29fd973b3cee46212164c37eed9
Matthew Jacobson <JacobsonMT@gmail.com>
2019-04-02 02:59:15 -0400

    Restore queue table state on page reload

--- src/main/resources/static/js/queue.js
+++ src/main/resources/static/js/queue.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
         "searching": false,
         "info": false,
         "order": [],
-        "lengthMenu": [ [5, 10, 25, 50, -1], [5, 10, 25, 50, "All"] ]
+        "lengthMenu": [ [5, 10, 25, 50, -1], [5, 10, 25, 50, "All"] ],
+        "stateSave": true,
     });
 });
