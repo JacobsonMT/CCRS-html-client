8699163721692347b91bca6a1e5e997142a3a8ca
Matthew Jacobson <JacobsonMT@gmail.com>
2019-07-20 17:05:03 -0400

    Improve security and escape CCRSService exchanges

--- src/main/java/com/jacobsonmt/ths/services/CCRSService.java
+++ src/main/java/com/jacobsonmt/ths/services/CCRSService.java
@@ -1,6 +1,5 @@
 package com.jacobsonmt.ths.services;
 
-import com.jacobsonmt.ths.model.Base;
 import com.jacobsonmt.ths.model.THSJob;
 import com.jacobsonmt.ths.settings.ApplicationSettings;
 import com.jacobsonmt.ths.settings.SiteSettings;
@@ -19,10 +18,7 @@ import org.springframework.web.client.DefaultResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Log4j2
 @Service
@@ -53,21 +49,21 @@ public class CCRSService {
         HttpEntity entity = new HttpEntity(createHeaders());
         // getForObject cannot specify headers so we use exchange
 
-        // TODO: this might be vulnerable to attack, either validate jobId or do some sort of escaping
         log.info( "Client: (" + applicationSettings.getClientId() + "), Job: (" + jobId + ")" );
         ResponseEntity<THSJob> response
-                = restTemplate.exchange(applicationSettings.getProcessServerURI() + "/job/" + jobId, HttpMethod.GET, entity, THSJob.class);
+                = restTemplate.exchange(
+                        applicationSettings.getProcessServerURI() + "/job/{jobId}",
+                HttpMethod.GET,
+                entity,
+                THSJob.class,
+                jobId
+        );
 
         // Add parsed version of result csv to aid in creation of front-end visualisations
         THSJob job = response.getBody();
 
         if (job != null && job.getResult() != null) {
-            List<Base> sequence = Arrays.stream( job.getResult().getResultCSV().split( "\\r?\\n" ) )
-                    .skip( 2 ) // Skip OX taxa id and header
-                    .map( mapBase )
-                    .collect( Collectors.toList() );
-            job.getResult().setSequence( sequence );
-
+            job.migrateCSVResultToSequence();
         }
 
         return response.getBody();
@@ -79,10 +75,14 @@ public class CCRSService {
         HttpEntity entity = new HttpEntity(createHeaders());
         // getForObject cannot specify headers so we use exchange
 
-        // TODO: this might be vulnerable to attack, either validate jobId or do some sort of escaping
         log.info( "Client: (" + applicationSettings.getClientId() + "), Job: (" + jobId + ")" );
         ResponseEntity<String> response
-                = restTemplate.exchange(applicationSettings.getProcessServerURI() + "/job/" + jobId + "/delete", HttpMethod.GET, entity, String.class);
+                = restTemplate.exchange(applicationSettings.getProcessServerURI() + "/job/{jobId}/delete",
+                HttpMethod.GET,
+                entity,
+                String.class,
+                jobId
+        );
 
         return response.getBody();
 
@@ -95,8 +95,12 @@ public class CCRSService {
 
         try {
             ResponseEntity<Integer> response
-                    = restTemplate.exchange( applicationSettings.getProcessServerURI() + "/queue/client/" + applicationSettings.getClientId() + "/complete",
-                    HttpMethod.GET, entity, Integer.class );
+                    = restTemplate.exchange( applicationSettings.getProcessServerURI() + "/queue/client/{clientId}/complete",
+                    HttpMethod.GET,
+                    entity,
+                    Integer.class,
+                    applicationSettings.getClientId()
+            );
 
             Integer newCompletionCount = response.getBody();
 
@@ -111,16 +115,31 @@ public class CCRSService {
         return false;
     }
 
-    public List<THSJob> getJobsForUser(String userId) {
+    public List<THSJob> getJobsForUser( String userId ) {
+        return getJobsForUser( userId, false );
+    }
+
+    public List<THSJob> getJobsForUser( String userId, boolean withResults ) {
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity entity = new HttpEntity(createHeaders());
         // getForObject cannot specify headers so we use exchange
 
-        // TODO: this might be vulnerable to attack, either validate jobId or do some sort of escaping
-        log.info( "Client: (" + applicationSettings.getClientId() + "), User: (" + userId + ")" );
-        ResponseEntity<List<THSJob>> response
-                = restTemplate.exchange(applicationSettings.getProcessServerURI() + "/queue/client/" + applicationSettings.getClientId() + "/user/" + userId,
-                HttpMethod.GET, entity, new ParameterizedTypeReference<List<THSJob>>(){});
+        log.info( "Get Jobs for Client: (" + applicationSettings.getClientId() + "), User: (" + userId + ")" );
+        ResponseEntity<List<THSJob>> response = restTemplate.exchange(
+                applicationSettings.getProcessServerURI() + "/queue/client/{clientId}/user/{userId}?withResults={withResults}",
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<THSJob>>(){},
+                applicationSettings.getClientId(),
+                userId,
+                withResults
+                );
+
+        if ( withResults && response.getBody() != null ) {
+            for ( THSJob job : response.getBody() ) {
+                job.migrateCSVResultToSequence();
+            }
+        }
 
         return response.getBody();
 
@@ -132,18 +151,6 @@ public class CCRSService {
         headers.set( "auth_token", applicationSettings.getClientToken() );
         return headers;
     }
-
-    private static Function<String, Base> mapBase = ( rawLine ) -> {
-        List<String> line = Arrays.asList( rawLine.split( "\t" ) );
-
-        Base base = new Base( line.get( 2 ), Integer.valueOf( line.get( 3 ) ), Double.valueOf( line.get( 4 ) ) );
-
-        if ( line.size() > 5 ) {
-            base.setList( line.stream().skip( 5 ).map( Double::parseDouble ).collect( Collectors.toList() ) );
-        }
-        return base;
-    };
-
 
     @ToString
     @Getter
