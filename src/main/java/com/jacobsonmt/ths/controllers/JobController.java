30c74e09113dcd6a4e0498f6f3e4840e9ded5e99
Matthew Jacobson <JacobsonMT@gmail.com>
2019-07-21 21:22:41 -0400

    Remove unused pages, move api docs to root uri

--- src/main/java/com/jacobsonmt/ths/controllers/JobController.java
+++ src/main/java/com/jacobsonmt/ths/controllers/JobController.java
@@ -4,16 +4,14 @@ import com.jacobsonmt.ths.exceptions.JobNotFoundException;
 import com.jacobsonmt.ths.model.Base;
 import com.jacobsonmt.ths.model.THSJob;
 import com.jacobsonmt.ths.services.CCRSService;
-import com.jacobsonmt.ths.utils.InputStreamUtils;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -25,56 +23,6 @@ public class JobController {
 
     @Autowired
     private CCRSService ccrsService;
-
-    @PostMapping("/")
-    public String submitJob(       @RequestParam(value = "fasta", required = false, defaultValue = "") String fasta,
-                                   @RequestParam(value = "fastaFile", required = false) MultipartFile fastaFile,
-//                                   @RequestParam(value = "label", required = false, defaultValue = "") String label,
-                                   @RequestParam(value = "email", required = false, defaultValue = "") String email,
-//                                   HttpServletRequest request,
-                                   RedirectAttributes redirectAttributes) throws IOException {
-
-//        String ipAddress = request.getHeader( "X-FORWARDED-FOR" );
-//        if ( ipAddress == null ) {
-//            ipAddress = request.getRemoteAddr();
-//        }
-
-        String userId = RequestContextHolder.currentRequestAttributes().getSessionId();
-
-        if ( fasta.isEmpty() ) {
-            if (fastaFile != null) {
-                fasta = InputStreamUtils.inputStreamToString( fastaFile.getInputStream() );
-            } else {
-                redirectAttributes.addFlashAttribute( "errorMessage", "FASTA Not Found" );
-                return "redirect:/";
-            }
-        }
-
-        ResponseEntity<CCRSService.JobSubmissionResponse> jobSubmissionResponse = ccrsService.submitJob( userId,
-                "",
-                fasta,
-                email,
-                true );
-
-        if ( jobSubmissionResponse.getStatusCodeValue() == 202 && jobSubmissionResponse.getBody() != null) {
-            List<String> jobIds = jobSubmissionResponse.getBody().getJobIds();
-            StringBuilder message;
-            if ( jobIds.size() > 1 ) {
-                message = new StringBuilder( "Multiple Jobs Submitted" );
-            } else {
-                message = new StringBuilder( "Job Submitted" );
-            }
-            redirectAttributes.addFlashAttribute( "submitMessage", message.toString() );
-            if (!jobSubmissionResponse.getBody().getMessage().isEmpty()) {
-                redirectAttributes.addFlashAttribute( "warnMessage", jobSubmissionResponse.getBody().getMessage() );
-            }
-        } else {
-            redirectAttributes.addFlashAttribute( "errorMessage",
-                    jobSubmissionResponse.getBody() != null ? jobSubmissionResponse.getBody().getMessage() : "Server Error" );
-        }
-
-        return "redirect:/";
-    }
 
     @GetMapping("/job/{jobId}")
     public String job( @PathVariable("jobId") String jobId,
@@ -119,11 +67,6 @@ public class JobController {
         }
 
         return new ArrayList<>();
-    }
-
-    @GetMapping("/job/{jobId}/delete")
-    public ResponseEntity<String> deleteJob( @PathVariable("jobId") String jobId ) {
-        return ccrsService.deleteJob( jobId );
     }
 
     @GetMapping("/job/{jobId}/resultCSV")
--- src/main/java/com/jacobsonmt/ths/controllers/MainController.java
+++ src/main/java/com/jacobsonmt/ths/controllers/MainController.java
@@ -1,101 +1,19 @@
 package com.jacobsonmt.ths.controllers;
 
-import com.jacobsonmt.ths.model.ContactForm;
-import com.jacobsonmt.ths.services.CCRSService;
-import com.jacobsonmt.ths.services.EmailService;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSendException;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.context.request.RequestContextHolder;
-
-import javax.mail.MessagingException;
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 @Log4j2
 @Controller
 public class MainController {
 
-    @Autowired
-    private CCRSService ccrsService;
-
-    @Autowired
-    private EmailService emailService;
-
-
     @GetMapping("/")
-    public String index( Model model) {
-        String userId = RequestContextHolder.currentRequestAttributes().getSessionId();
-        model.addAttribute("jobs", ccrsService.getJobsForUser( userId ).getBody());
-        return "index";
-    }
-
-    @GetMapping("/job-table")
-    public String getJobTable( Model model) {
-        String userId = RequestContextHolder.currentRequestAttributes().getSessionId();
-        model.addAttribute("jobs", ccrsService.getJobsForUser( userId ).getBody());
-
-        return "index :: #job-table";
-    }
-
-    @GetMapping("/queue")
-    public String queue( Model model) {
-        String userId = RequestContextHolder.currentRequestAttributes().getSessionId();
-        model.addAttribute("jobs", ccrsService.getJobsForUser( userId ).getBody());
-
-        return "queue";
-    }
-
-    @GetMapping("/documentation")
-    public String documentation( Model model) {
-        return "documentation";
-    }
-
-    @GetMapping("/contact")
-    public String contact( Model model) {
-        model.addAttribute("contactForm", new ContactForm());
-        return "contact";
-    }
-
-    @PostMapping("/contact")
-    public String contact( Model model,
-                           HttpServletRequest request,
-                           @Valid ContactForm contactForm,
-                           BindingResult bindingResult ) {
-        if (bindingResult.hasErrors()) {
-            return "contact";
-        }
-
-        log.info( contactForm );
-        try {
-            emailService.sendSupportMessage( contactForm.getMessage(), contactForm.getName(), contactForm.getEmail(), request, contactForm.getAttachment() );
-            model.addAttribute( "message", "Sent. We will get back to you shortly." );
-            model.addAttribute( "success", true );
-        } catch ( MessagingException | MailSendException e) {
-            log.error(e);
-            model.addAttribute( "message", "There was a problem sending the support request. Please try again later." );
-            model.addAttribute( "success", false );
-        }
-
-        return "contact";
-    }
-
-    @GetMapping("/maintenance")
-    public String contact() {
-        return "maintenance";
-    }
-
-    @GetMapping("/api-docs")
     public String test() {
         return "docs/swagger-ui.html";
     }
 
-    @GetMapping("/api-docs/swagger-ui.html")
+    @GetMapping("/swagger-ui.html")
     public String swagger() {
         return "docs/swagger-ui.html";
     }
