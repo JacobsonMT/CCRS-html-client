56e5aa352592d9c861cb5c382047dfe5374e5917
Matthew Jacobson <JacobsonMT@gmail.com>
2019-08-11 19:47:35 -0400

    Fix 404 in JobController ccrsService.getJob

--- src/main/java/com/jacobsonmt/ths/controllers/JobController.java
+++ src/main/java/com/jacobsonmt/ths/controllers/JobController.java
@@ -7,6 +7,7 @@ import com.jacobsonmt.ths.services.CCRSService;
 import com.jacobsonmt.ths.utils.InputStreamUtils;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -69,13 +70,13 @@ public class JobController {
     public String job( @PathVariable("jobId") String jobId,
                        Model model) throws IOException {
 
-        THSJob job = ccrsService.getJob( jobId ).getBody();
+        ResponseEntity<THSJob> entity = ccrsService.getJob( jobId );
 
-        if (job==null) {
+        if (entity.getStatusCode().equals( HttpStatus.NOT_FOUND ) || entity.getBody() == null ) {
             throw new JobNotFoundException();
         }
 
-        model.addAttribute("job", job);
+        model.addAttribute("job", entity.getBody() );
 
         return "job";
     }
@@ -83,28 +84,30 @@ public class JobController {
     @GetMapping("/job/{jobId}/content")
     public String getJobViewContent( @PathVariable("jobId") String jobId,
                                      Model model) {
-        THSJob job = ccrsService.getJob( jobId ).getBody();
+        ResponseEntity<THSJob> entity = ccrsService.getJob( jobId );
 
-        if (job==null) {
+        if (entity.getStatusCode().equals( HttpStatus.NOT_FOUND ) || entity.getBody() == null ) {
             throw new JobNotFoundException();
         }
 
-        model.addAttribute("job", job );
+        model.addAttribute("job", entity.getBody() );
 
         return "job :: #job-view-content";
     }
 
-    @GetMapping(value = "/job/{jobId}/sequence", produces = "application/json")
+    @GetMapping(value = "/job/{jobId}/bases", produces = "application/json")
     @ResponseBody
-    public List<Base> getJobResultSequence( @PathVariable("jobId") String jobId) {
-        THSJob job = ccrsService.getJob( jobId ).getBody();
+    public List<Base> getJobResultBases( @PathVariable("jobId") String jobId) {
+        ResponseEntity<THSJob> entity = ccrsService.getJob( jobId );
 
-        if (job==null) {
+        if (entity.getStatusCode().equals( HttpStatus.NOT_FOUND ) || entity.getBody() == null ) {
             throw new JobNotFoundException();
         }
 
+        THSJob job = entity.getBody();
+
         if ( job.isComplete() && !job.isFailed() ) {
-            return job.getResult().getSequence();
+            return job.getResult().getBases();
         }
 
         return new ArrayList<>();
