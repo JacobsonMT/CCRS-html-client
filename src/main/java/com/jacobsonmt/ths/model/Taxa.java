dda91c537e068a2b0c491909e30939fc2bddce54
Matthew Jacobson <JacobsonMT+github@gmail.com>
2020-03-10 22:28:01 -0400

    Support CCRS taxa species name

--- src/main/java/com/jacobsonmt/ths/model/Taxa.java
+++ src/main/java/com/jacobsonmt/ths/model/Taxa.java
@@ -11,10 +11,16 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Taxa {
+
     @ApiModelProperty( notes = "Indication of how the given Taxa ID was determined. 'OX' if successful, otherwise a more specific key.",
             example = "invalid_OX" )
     private String key;
+
     @ApiModelProperty( notes = "Supplied or guessed OX Taxa ID for sequence, -1 if unknown or not supported",
             example = "9749" )
     private int id;
+
+    @ApiModelProperty( notes = "Species name for OX Taxa ID, empty if unknown or not supported",
+            example = "Delphinapterus leucas" )
+    private String name;
 }
