6765e25381a68cbd398365797bc33c50a97c91ca
Matthew Jacobson <JacobsonMT+github@gmail.com>
2020-03-10 22:40:23 -0400

    Remove unused ApiModelProperty

--- src/main/java/com/jacobsonmt/ths/model/THSJobResult.java
+++ src/main/java/com/jacobsonmt/ths/model/THSJobResult.java
@@ -14,8 +14,6 @@ import java.util.List;
 @NoArgsConstructor
 public final class THSJobResult {
 
-    @ApiModelProperty( notes = "Supplied or guessed OX Taxa ID for sequence, -1 if unknown or not supported",
-            example = "9749" )
     private Taxa taxa;
     @ApiModelProperty( notes = "Result row for each amino acid position in supplied FASTA, in order",
             example = "[{\"reference\":\"M\",\"depth\":41,\"conservation\":0.785,\"list\":[0.785,0.785,0.785,0.785,0.785,0.785,0.785,0.785,0.785,0.785,0.785,0.785,0.0,0.785,0.785,0.785,0.785,0.785,0.785,0.785]}]" )
